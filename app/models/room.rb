07251cdda6b0ff25e53729efe83773daf2a53931
yu10290430 <my10290430@icloud.com>
2022-02-24 05:48:48 +0000

    質問前の更新

--- app/models/room.rb
+++ app/models/room.rb
@@ -8,5 +8,13 @@ class Room < ApplicationRecord
   validates :adress, presence: true
   validates :room_image, presence:true
   
- 
+   def self.search(area)
+       Room.where(['adress LIKE ?', "%#{area}%"]) 
+   end 
+   
+   def self.search(key)
+       Room.where(['room_name LIKE ? OR adress LIKE ? OR price LIKE ?', "%#{key}%", "%#{key}%", "%#{key}%"]) 
+   end
+   
+  
 end
