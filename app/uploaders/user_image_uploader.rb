cfdd49cfa5c28e7635c7a38c27b715e71b6c9c1d
yu10290430 <my10290430@icloud.com>
2022-03-01 04:54:36 +0000

    提出

--- app/uploaders/user_image_uploader.rb
+++ app/uploaders/user_image_uploader.rb
@@ -29,4 +29,11 @@ class UserImageUploader < CarrierWave::Uploader::Base
       name.downcase
     end
   end
+  
+   def default_url(*args)
+  #   For Rails 3.1+ asset pipeline compatibility:
+   # ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default.png"].compact.join('_'))
+      "image_none.jpeg"
+  #   "/images/fallback/" + [version_name, "default.png"].compact.join('_')
+   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,19 @@
 Rails.application.routes.draw do
-
+ 
+ resources :users, only: [:edit, :update]
+ get 'users/account'
+ get 'users/profile'
  devise_for :users
   devise_scope :users do
     get '/users', to: redirect("/users/sign_up")
   end 
- 
-  get 'users/account'
-  get 'users/profile'
- resources :user, only: [:new, :create, :edit, :show, :update]
- 
   
   get 'rooms/search'
   resources :rooms, only: [ :index, :new, :show, :create]
   
   post 'reservations/confirm' => 'reservations#confirm'
   post 'reservations/back'
-  resources :reservations, only: [:index, :new, :create, :show]
-  
+  resources :reservations, only: [:index, :new, :create, :show] 
  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
  resources :posts
